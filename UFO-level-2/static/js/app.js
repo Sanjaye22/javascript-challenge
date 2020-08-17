fdfc95a67b56f1a5b9ce0c5319ef99df8dfa6f18
Sanjaye Thomas <sanjaye22@gmail.com>
2020-08-17 00:12:38 -0400

    updates

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -2,3 +2,18 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody")
+console.log(data);
+
+// Use d3 to update each cell's text with ufo sighting
+data.forEach(function(ufoSight) {
+  console.log(ufoSight);
+  var row = tbody.append("tr");
+  Object.entries(ufoSight).forEach(function([key, value]) {
+    console.log(key, value);
+    // Append a cell to the row for each value
+    // in the weather report object
+    var cell = row.append("td");
+    cell.text(value);
+  });
+});
